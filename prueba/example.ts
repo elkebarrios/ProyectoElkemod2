83d0ad60a665adcce219f308ca0936312da35d6c
elkebarrios <elkebarrios@gmail.com>
2022-10-12 14:57:24 -0300

    avance portfolio dashboard

---
+++ prueba/example.ts
@@ -0,0 +1,6 @@
+let a: number;
+let b: string;
+let c=101;
+c = "one";
+let recursos= ['memoria', 'disco', 100];
+let recursos: string[] = ['memoria', 'disco','procesador'];
---
+++ typescript/app.ts
@@ -0,0 +1,5 @@
+let message: string = 'Hello world!';
+console.log(message);
+
+
+
